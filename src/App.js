102c07c7ad22d3b5a78e956de29f113034373bce
mnaofficial <mna.official92@gmail.com>
2022-07-24 11:26:02 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Ecommerce Bazar.com</h2>
+      <p>An Online Shopping website</p>
     </div>
   );
 }
