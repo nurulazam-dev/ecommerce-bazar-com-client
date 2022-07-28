3ae30ae91eb49f0f6013378f64f910622f416e28
mnaofficial <mna.official92@gmail.com>
2022-07-28 21:39:10 +0600

    provider setup in index.js

--- src/index.js
+++ src/index.js
@@ -7,13 +7,15 @@ import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import '../node_modules/bootstrap/dist/js/bootstrap.bundle';
 import '../node_modules/font-awesome/css/font-awesome.min.css';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './redux/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
-    <React.StrictMode>
+    <Provider store={store}>
       <App />
-    </React.StrictMode>
+    </Provider>
   </BrowserRouter>
 );
 
