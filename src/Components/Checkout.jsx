596f39dcaf4806220cd2e0fe770f0807b7994964
mnaofficial <mna.official92@gmail.com>
2022-07-29 10:51:07 +0600

    checkout page setup complete

--- src/Components/Checkout.jsx
+++ src/Components/Checkout.jsx
@@ -1,6 +1,21 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 const Checkout = () => {
+    const state = useSelector((state) => state.addItem)
+    var total = 0;
+    const itemList = (item) => {
+        total = total + item?.price;
+        return (
+            <li className="list-group-item d-flex justify-content-between lh-sm">
+                <div>
+                    <h6 className="my-0">{item?.phone_name}</h6>
+                </div>
+                <span className="text-muted">$ {item?.price}</span>
+            </li>
+        )
+    }
+
     return (
         <>
             <div className="container my-3 py-3">
@@ -8,40 +23,15 @@ const Checkout = () => {
                     <div className="col-md-5 col-lg-4 order-md-last">
                         <h4 className="d-flex justify-content-between align-items-center mb-3">
                             <span className="text-primary">Your cart</span>
-                            <span className="badge bg-primary rounded-pill">3</span>
+                            <span className="badge bg-primary rounded-pill">{state?.length}</span>
                         </h4>
                         <ul className="list-group mb-3">
-                            <li className="list-group-item d-flex justify-content-between lh-sm">
-                                <div>
-                                    <h6 className="my-0">Product name</h6>
-                                    <small className="text-muted">Brief description</small>
-                                </div>
-                                <span className="text-muted">$12</span>
-                            </li>
-                            <li className="list-group-item d-flex justify-content-between lh-sm">
-                                <div>
-                                    <h6 className="my-0">Second product</h6>
-                                    <small className="text-muted">Brief description</small>
-                                </div>
-                                <span className="text-muted">$8</span>
-                            </li>
-                            <li className="list-group-item d-flex justify-content-between lh-sm">
-                                <div>
-                                    <h6 className="my-0">Third item</h6>
-                                    <small className="text-muted">Brief description</small>
-                                </div>
-                                <span className="text-muted">$5</span>
-                            </li>
-                            <li className="list-group-item d-flex justify-content-between bg-light">
-                                <div className="text-success">
-                                    <h6 className="my-0">Promo code</h6>
-                                    <small>EXAMPLECODE</small>
-                                </div>
-                                <span className="text-success">−$5</span>
-                            </li>
+                            {
+                                state?.map(itemList)
+                            }
                             <li className="list-group-item d-flex justify-content-between">
                                 <span>Total (USD)</span>
-                                <strong>$20</strong>
+                                <strong>$ {total}</strong>
                             </li>
                         </ul>
 
