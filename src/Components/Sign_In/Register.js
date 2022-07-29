5f03e12f5bb9943fb648f23961f3374f91a271c3
mnaofficial <mna.official92@gmail.com>
2022-07-30 01:16:49 +0600

    register page design modify

--- src/Components/Sign_In/Register.js
+++ src/Components/Sign_In/Register.js
@@ -3,6 +3,7 @@ import { Link, useNavigate } from 'react-router-dom';
 import { useCreateUserWithEmailAndPassword, useSendEmailVerification } from 'react-firebase-hooks/auth';
 import auth from '../../Firebase/Firebase.init';
 import SignInWithSocial from './SocialLogin';
+import login_img from "../../Assets/Social Logo/login_img.webp"
 
 const Register = () => {
     const [email, setEmail] = useState('');
@@ -41,40 +42,43 @@ const Register = () => {
     if (user) {
         navigate('/login')
     }
-   
+
 
     return (
-        <div style={{ width: "500px" }} className='container mx-auto border p-3 rounded mt-5 shadow'>
-            <h2 className='text-primary text-center mt-2'>Please Register</h2>
-            <form onSubmit={handleRegister} className='w-75 mx-auto'>
-                <div className="mb-3">
-                    <input type="text" className="form-control fs-6" id="exampleInputName1" placeholder='Your Name' />
+        <div className='container bg-white mx-auto border p-3 rounded mt-5 shadow'>
+            <div className="row py-4">
+                <h2 className='text-primary text-center pb-4'>Please Register</h2>
+                <div className="col-md-5 mx-4">
+                    <img src={login_img} alt="About Us" height='460px' width='500px' />
                 </div>
-                <div className="mb-3">
-                    <input type="email" onBlur={handleEmail} className="form-control fs-6" id="exampleInputEmail1" placeholder='Email Address' required />
+                <div className="col-md-6">
+                    <form onSubmit={handleRegister} className='w-75 mx-auto'>
+                        <div className="mb-3">
+                            <input type="text" className="p-2 form-control fs-5" id="exampleInputName1" placeholder='Your Name' />
+                        </div>
+                        <div className="mb-3">
+                            <input type="email" onBlur={handleEmail} className="p-2 form-control fs-5" id="exampleInputEmail1" placeholder='Email Address' required />
+                        </div>
+                        <div className="mb-3">
+                            <input type="password" onBlur={handlePassword} className="p-2 form-control fs-5" id="exampleInputPassword1" placeholder='Password' required />
+                        </div>
+                        <div className="mb-3">
+                            <input type="password" onBlur={handleConfirmPassword} className="p-2 form-control fs-5" id="exampleInputConfirmPassword1" placeholder='Confirm Password' required />
+                        </div>
+                        <p className='text-danger'>{error}</p>
+
+                        <button type="submit" className="btn btn-primary w-100 mx-auto fs-5"
+                            onClick={(() => createUserWithEmailAndPassword(email, password)) &&
+                                (async () => {
+                                    await sendEmailVerification();
+                                    alert('Sent email');
+                                })}
+                        >Register</button>
+                    </form>
+                    <p className='text-center mt-3 mb-0'>If you have already account ? <Link className='text-info text-decoration-none' to='/login'>Please Login</Link> </p>
+                    <SignInWithSocial></SignInWithSocial>
                 </div>
-                <div className="mb-3">
-                    <input type="password" onBlur={handlePassword} className="form-control fs-6" id="exampleInputPassword1" placeholder='Password' required />
-                </div>
-                <div className="mb-3">
-                    <input type="password" onBlur={handleConfirmPassword} className="form-control fs-6" id="exampleInputConfirmPassword1" placeholder='Confirm Password' required />
-                </div>
-                <p className='text-danger'>{error}</p>
-                
-                <button type="submit" className="btn btn-primary w-100 mx-auto fs-5"
-                    onClick={(() => createUserWithEmailAndPassword(email, password)) &&
-                        (async () => {
-                            await sendEmailVerification();
-                            alert('Sent email');
-                        })}
-                >Register</button>
-            </form>
-
-
-
-             <p className='text-center mt-3'>If you have already account ? <Link className='text-primary text-decoration-none' to='/login'>Please Login</Link> </p> 
-            <SignInWithSocial></SignInWithSocial>
-
+            </div>
         </div>
     );
 };
