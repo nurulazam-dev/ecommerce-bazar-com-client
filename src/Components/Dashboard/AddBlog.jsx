de01fc0553ca50fbd835467cae8ea023ee9248cd
mnaofficial <mna.official92@gmail.com>
2022-08-06 00:25:51 +0600

    dashboard sub-router page create

---
+++ src/Components/Dashboard/AddBlog.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AddBlog = () => {
+    return (
+        <div>
+            <h2>Add Blog</h2>
+        </div>
+    );
+};
+
+export default AddBlog;
---
+++ src/Components/Dashboard/Analysis.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Analysis = () => {
+    return (
+        <div>
+            <h2>Analysis</h2>
+        </div>
+    );
+};
+
+export default Analysis;
